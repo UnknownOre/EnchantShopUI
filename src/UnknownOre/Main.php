01a8846811bfedfab155075cb7c3ece97f4ba03a
Unknown <47182802+UnknownOre@users.noreply.github.com>
2019-02-10 18:36:35 +0200

    fixed bugs + you can only enchant swords (for now)

--- src/UnknownOre/Main.php
+++ src/UnknownOre/Main.php
@@ -10,6 +10,7 @@ use pocketmine\plugin\PluginBase as PB;
 
 use onebone\economyapi\EconomyAPI;
 use joe777777\FormAPI;
+use pocketmine\utils\TextFormat as C;
 
 class Main extends PB {
     public function onEnable(): void{
@@ -25,8 +26,8 @@ class Main extends PB {
     }
     public function ListForm(Player $player): void{
         $form = $this->fapi->createSimpleForm(function (Player $player, $data = null){
-            if ($data === null) {
-                
+            if ($data === null){
+                $player->sendMessage(C::GREEN.'Thank You for using Enchant Shop');
             }else{
                 $this->BuyForm($player, $data);
             }
@@ -34,37 +35,42 @@ class Main extends PB {
         $array = $this->shop->getAll();
 		foreach ($array as $name => $content) {
 
-			$form->addButton($content[0]." ".$content[2]."$");
+			$form->addButton(C::DARK_AQUA.$content[0]." ".C::GREEN.$content[2]."$");
 		}
-        $form->setTitle("§cEnchantment §aShop");
+        $form->setTitle(C::RED."Enchantment Shop");
         $form->sendToPlayer($player);
     }
     public function BuyForm(Player $player, $id): void{
         $array = $this->shop->getAll();
-      
         $price = $array[$id][2];
-        $name = $array[$id][0];
         $ide = $array[$id][1];
         $level = $array[$id][3];
-      
+        $name = $array[$id][0];
         $form = $this->fapi->createCustomForm(function (Player $player, $data = null) use ($price, $ide, $name){
             $money = EconomyAPI::getInstance()->myMoney($player);
-            if($data === null){
+            $fprice = $price * $data[1];
+            $swordids = array(267, 268, 272, 276, 283);
+            if ($data === null){
                 $this->ListForm($player);
-            }elseif($money > $price * $data[0]){
-                EconomyAPI::getInstance()->reduceMoney($player,  $price);
-                $item = $player->getInventory()->getItemInHand();
-                $ench = Enchantment::getEnchantment($ide);
-                $item->addEnchantment(new EnchantmentInstance($ench, (int) $data[0]));
-                $player->getInventory()->setItemInHand($item);
-                $player->sendMessage('§ayou have bought '.$name.' level ' .$data[0]. 'for '.$price * $data[0]."$");
+            }elseif(in_array($player->getInventory()->getItemInHand()->getId(), $swordids)) {
+                if($money > $fprice){
+                    EconomyAPI::getInstance()->reduceMoney($player,  $price * $data[1]);
+                    $item = $player->getInventory()->getItemInHand();
+                    $ench = Enchantment::getEnchantment($ide);
+                    $item->addEnchantment(new EnchantmentInstance($ench, (int) $data[1]));
+                    $player->getInventory()->setItemInHand($item);
+                    $message = C::GREEN."You have bought ". $name. " level ". $data[1]. " For ".C::YELLOW. $fprice.C::RED. "$";
+                    $player->sendMessage($message);
+                }else{
+                    $player->sendMessage(C::RED.' You dont have enough Money!');
+                }
             }else{
-                $player->sendMessage('§cyou dont have enough Money');
+                $player->sendMessage(C::RED.'Hold a Sword!');
             }
         }
         );
-        $form->setTitle("§eBuy enchantment");
-        $form->addLabel("§aEnchantment:§c ".$name."\n§aYou will pay §e".$price."$ §aper level");
+        $form->addLabel(C::DARK_AQUA.'You will pay '.C::YELLOW. $price.'$'.C::DARK_AQUA.' per level');
+        $form->setTitle(C::RED."Enchantment Shop");
         $form->addSlider("Level", 1, $level, 1, -1);
         $form->sendToPlayer($player);
     }
