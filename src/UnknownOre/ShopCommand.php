2363e76c304db12875619f366334047d6a4e7085
Unknown <47182802+UnknownOre@users.noreply.github.com>
2019-02-10 19:28:53 +0200

    Update ShopCommand.php

--- src/UnknownOre/ShopCommand.php
+++ src/UnknownOre/ShopCommand.php
@@ -1,10 +1,9 @@
 <?php
 namespace UnknownOre;
 
-use pocketmine\command\Command;
-use pocketmine\command\PluginCommand;
-use pocketmine\command\CommandSender;
+use pocketmine\command\{ Command, PluginCommand, CommandSender};
 use pocketmine\Player;
+
 class ShopCommand extends PluginCommand {
     
     public function __construct(Main $plugin) {
@@ -13,8 +12,13 @@ class ShopCommand extends PluginCommand {
         $this->setDescription('Main Enchant command');
         $this->plugin = $plugin;
     }
+	
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        $this->plugin->ListForm($sender);
+        if ($sender instanceof Player) {
+            $this->plugin->ListForm($sender);
+        }else{
+            $sender->sendMessage(TF::RED . "Please use this in-game.");
+        }
 		return true;
 	}
     
