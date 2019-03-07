3dbbaccb4d8c48fb874525948adfbc7351937c2d
Unknown <47182802+UnknownOre@users.noreply.github.com>
2019-03-07 21:45:33 +0200

    Update 0.4
    -Customise Shop Messages in Config
    -PiggyCE support
    -new config format

--- src/UnknownOre/EnchantUI/Main.php
+++ src/UnknownOre/EnchantUI/Main.php
@@ -8,6 +8,8 @@ use pocketmine\{
 };
 use pocketmine\item\{
     Item,
+    Tool,
+    Armor,
     enchantment\Enchantment,
     enchantment\EnchantmentInstance
 };
@@ -24,16 +26,18 @@ class Main extends PluginBase{
     
     public function onEnable(): void{
         @mkdir($this->getDataFolder());
-        if(!file_exists($this->getDataFolder() . "Shop.yml")) {
+        $this->getLogger()->notice("EnchantShopUI has been enabled.");
+        $this->shop = new Config($this->getDataFolder() . "Shop.yml", Config::YAML);
+        if(is_null($this->shop->getNested('version'))){
             file_put_contents($this->getDataFolder() . "Shop.yml",$this->getResource("Shop.yml"));
+            $this->getLogger()->notice("Updating Plugin Config.....");
         }
         $this->saveDefaultConfig();
         $this->getServer()->getCommandMap()->register("enchantui", new Commands\ShopCommand($this));
-        $this->shop = new Config($this->getDataFolder() . "Shop.yml", Config::YAML);
         $this->piggyCE = $this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
     }
-	
-    /**
+    
+	/**
     * @param Player $player
     */
     public function listForm(Player $player): void{
@@ -54,8 +58,8 @@ class Main extends PluginBase{
         $form->setTitle($this->shop->getNested('Title'));
         $player->sendForm($form);
     }
-	
-    /**
+    
+	/**
     * @param Player $player
     * @param int $id
     */
@@ -72,9 +76,13 @@ class Main extends PluginBase{
                 $this->listForm($player);
                 return;
             }
+            if(!$player->getInventory()->getItemInHand() instanceof Tool and !$player->getInventory()->getItemInHand() instanceof Armor){
+                $player->sendMessage($this->shop->getNested('messages.hold-item'));
+                return;
+            }
             if(EconomyAPI::getInstance()->myMoney($player) > $c = $array[$id]['price'] * $data[1]){
                 EconomyAPI::getInstance()->reduceMoney($player, $c);
-                $this->enchantItem($array[$id]['enchantment'], $player,$data[1]); 
+                $this->enchantItem($player, $data[1], $array[$id]['enchantment']); 
                 $player->sendMessage($this->replace($this->shop->getNested('messages.paid-success'), $var));
             }else{
                 $player->sendMessage($this->replace($this->shop->getNested('messages.not-enough-money'), $var));
@@ -86,15 +94,17 @@ class Main extends PluginBase{
         $form->addSlider($this->shop->getNested('slider-title'), 1, $array[$id]['max-level'], 1, -1);
         $player->sendForm($form);
     }
+    
     /**
-    * @param string|int $enchantment
     * @param Player $Item
+    * @param int $level
+    * @param int|String $enchantment
     */
-	public function enchantItem($enchantment ,Player $player,int $level): void{
+	public function enchantItem(Player $player, int $level, $enchantment): void{
         $item = $player->getInventory()->getItemInHand();
         if(is_string($enchantment)){
             $ench = Enchantment::getEnchantmentByName((string) $enchantment);
-            if($ench === null){
+            if($this->piggyCE !== null && $ench === null){
                 $ench = CustomEnchants::getEnchantmentByName((string) $enchantment);
             }
             if($this->piggyCE !== null && $ench instanceof CustomEnchants){
@@ -109,6 +119,7 @@ class Main extends PluginBase{
         }
         $player->getInventory()->setItemInHand($item);
     }
+    
     /**
     * @param string $message
     * @param array $keys
