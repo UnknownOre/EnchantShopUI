97628090b626c46e3b61ba4cf504089764967806
Unknown <47182802+UnknownOre@users.noreply.github.com>
2019-03-07 18:40:12 +0200

    new update 0.4

--- src/UnknownOre/EnchantUI/ShopCommand.php
+++ src/UnknownOre/EnchantUI/ShopCommand.php
@@ -1,25 +1,55 @@
 <?php
-namespace UnknownOre\EnchantUI;
+namespace UnknownOre\EnchantUI\Commands;
 
-use pocketmine\command\{ Command, PluginCommand, CommandSender};
+use pocketmine\command\{
+    Command,
+    PluginCommand,
+    CommandSender
+};
+use pocketmine\item\{
+    Item,
+    Tool,
+    Armor
+};
 use pocketmine\Player;
+use UnknownOre\EnchantUI\Main;
 
 class ShopCommand extends PluginCommand {
     
+    /**
+     * ShopCommand constructor.
+     * @param Main $plugin
+     */
     public function __construct(Main $plugin) {
         parent::__construct('enchantui', $plugin);
         $this->setAliases(['eshop','es']);
         $this->setDescription('Main Enchant command');
+        $this->setPermission("eshop.command");
         $this->plugin = $plugin;
     }
-	
+    
+	/**
+    * @param CommandSender $sender
+    * @param string $commandLabel
+    * @param array $args
+    *
+    * @return bool
+    */
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
-        if ($sender instanceof Player) {
-            $this->plugin->ListForm($sender);
-        }else{
-            $sender->sendMessage("Please use this in-game.");
+        if(!$sender->hasPermission("eshop.command")){
+            $sender->sendMessage($this->plugin->shop->getNested('messages.hold-item'));
+            return false;
         }
-		return true;
+        if(!$sender instanceof Player){
+            $sender->sendMessage("Please use this in-game.");
+            return false;
+        }
+        if(!$sender->getInventory()->getItemInHand() instanceof Tool and !$sender->getInventory()->getItemInHand() instanceof Armor){
+            $sender->sendMessage($this->plugin->shop->getNested('messages.hold-item'));
+            return false;
+        }   
+        $this->plugin->listForm($sender);
+        return true;
 	}
     
 }
