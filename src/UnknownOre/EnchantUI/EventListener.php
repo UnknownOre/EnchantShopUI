7abad829809193fb0e0eb695e7b89978cc4ee681
Unknown <47182802+UnknownOre@users.noreply.github.com>
2019-03-30 15:56:51 +0200

    Update EventListener.php

--- src/UnknownOre/EnchantUI/EventListener.php
+++ src/UnknownOre/EnchantUI/EventListener.php
@@ -6,25 +6,24 @@ use pocketmine\event\Listener;
 use pocketmine\Item;
 use pocketmine\block\EnchantingTable;
 Class EventListener implements Listener{
-    
     /** @var EnchantUI */
     private $plugin;
     
     /**
-     * EventListener constructor.
-     * @param EnchantUI $plugin
-     */
-	public function __construct(Main $plugin){
+    * EventListener constructor.
+    * @param EnchantUI $plugin
+    */
+    public function __construct(Main $plugin){
 		$this->plugin = $plugin;
 	}
     
     /**
-	 * @param PlayerInteractEvent $ev
-	 */
+    * @param PlayerInteractEvent $ev
+    */
     public function onInteract(PlayerInteractEvent $ev){
-        $block = $ev->getBlock();
-        $table = $this->plugin->shop->getNested('enchantment-table');
-        if($table and $block instanceof EnchantingTable){
+        if($ev->getAction() !== PlayerInteractEvent::RIGHT_CLICK_BLOCK) return;
+        $table = $this->plugin->shop->getNested('enchanting-table');
+        if($table and $ev->getBlock() instanceof EnchantingTable){
             $ev->setCancelled();
             $this->plugin->ListForm($ev->getPlayer());
         }
